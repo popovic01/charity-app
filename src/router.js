2e72a62056499a4b56c631c260bc6c8b85b82dc1
Milica <milica.popovic@studenti.unipd.it>
2024-08-16 12:53:23 +1000

    feat: add route guard

--- src/router.js
+++ src/router.js
@@ -4,7 +4,14 @@ import SignUp from './components/SignUp.vue'
 import SignIn from './components/SignIn.vue'
 
 const routes = [
-  { path: '/', component: Home },
+  {
+    path: '/',
+    component: Home
+    // add this if you want to allow access only to auth users
+    // meta: {
+    //   requiresAuth: true
+    // }
+  },
   { path: '/sign-in', component: SignIn },
   { path: '/sign-up', component: SignUp }
 ]
@@ -12,6 +19,21 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes: routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth) {
+    if (localStorage.getItem('currentUser')) {
+      // user is authenticated, proceed to the route
+      next()
+    } else {
+      // uer is not authenticated, redirect to login
+      next('/sign-in')
+    }
+  } else {
+    // Non-protected route, allow access
+    next()
+  }
 })
 
 export default router
