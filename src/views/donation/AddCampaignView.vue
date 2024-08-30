fff61893374cb2518bd73275d4bb2cf910308d02
Milica <milica.popovic@studenti.unipd.it>
2024-08-30 13:52:15 +1000

    style: improve css for add campaign

--- src/views/donation/AddCampaignView.vue
+++ src/views/donation/AddCampaignView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container mt-5">
+  <div class="container mt-5 mb-3">
     <div class="row">
       <div class="col-md-8 offset-md-2">
         <h1 class="mb-3">Add donation campaign</h1>
