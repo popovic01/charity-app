417e6c9d16d66397ef96f100351def3e9d0e74f8
Milica <milica.popovic@studenti.unipd.it>
2024-09-11 12:09:05 +1000

    feat: add home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,1 +1,7 @@
-<template>home</template>
+<template>
+  <CoverImage :title="'Indigenous Health Hub Foundation'"></CoverImage>
+</template>
+
+<script setup>
+import CoverImage from '../components/CoverImage.vue'
+</script>
