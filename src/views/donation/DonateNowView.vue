73360af18e5c9864cba4e1dc8da2e071a8430a87
Milica <milica.popovic@studenti.unipd.it>
2024-09-11 12:08:55 +1000

    feat: add email sending

--- src/views/donation/DonateNowView.vue
+++ src/views/donation/DonateNowView.vue
@@ -1,9 +1,115 @@
 <template>
+  <Toast />
   <CoverImage :title="'Donate Now'"></CoverImage>
+
+  <div class="container mt-5 mb-3">
+    <div class="row">
+      <div class="col-md-8 offset-md-2">
+        <h1 class="mb-3">Donate now</h1>
+
+        <form @submit.prevent="submitForm" id="donateForm">
+          <div class="row mb-3">
+            <div class="col-md-7 mb-2">
+              <label for="amount" class="form-label">Amount</label>
+              <input
+                type="number"
+                min="5"
+                class="form-control"
+                id="amount"
+                v-model="formData.amount"
+                @input="() => validateAmount()"
+              />
+              <p v-if="!formValidation.isAmount && isFormSubmitted" class="text-danger">
+                Amount is required.
+              </p>
+            </div>
+          </div>
+          <div class="text-center">
+            <button type="submit" class="btn btn-primary me-2">Donate</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
 import CoverImage from '../../components/CoverImage.vue'
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import emailjs from 'emailjs-com'
+import { useToast } from 'primevue/usetoast'
+import Toast from 'primevue/toast'
+
+const toast = useToast()
+
+const showMessage = (title, description, severity) => {
+  toast.add({
+    severity: severity,
+    summary: title,
+    detail: description,
+    life: 2000
+  })
+}
+const router = useRouter()
+
+const isFormSubmitted = ref(false)
+
+const getInitialData = () => ({
+  amount: 5
+})
+
+const getInitialValidation = () => ({
+  isAmountValid: false
+})
+
+const formData = ref(getInitialData())
+const formValidation = ref(getInitialValidation())
+
+const submitForm = () => {
+  isFormSubmitted.value = true
+  validateAmount()
+
+  //the validation is correct
+  if (formValidation.value.isAmountValid) {
+    sendEmail()
+    // router.push('/')
+    clearForm()
+  }
+}
+
+function sendEmail() {
+  const email = JSON.parse(localStorage.getItem('currentUser')).email
+  try {
+    emailjs.send(
+      'service_mzrtc4i',
+      'template_dnnddu7',
+      {
+        to_name: email.substring(0, email.indexOf('@')),
+        to_email: email,
+        amount: formData.value.amount
+      },
+      'LsataIKg8wAhradmK'
+    )
+    showMessage('Successfull donation', 'Thank You for Your Generous Donation', 'success')
+  } catch (error) {
+    showMessage('An error occured', error, 'error')
+  }
+}
+
+function validateAmount() {
+  if (formData.value.amount) {
+    formValidation.value.isAmountValid = true
+  } else {
+    formValidation.value.isAmountValid = false
+  }
+}
+
+function clearForm() {
+  formData.value = getInitialData()
+  formValidation.value = getInitialValidation()
+  isFormSubmitted.value = false
+}
 </script>
 
 <style></style>
